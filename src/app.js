e53f7610b79c3e41d4699ad6e9b6ce559334837d
CarlosCF0503 <carlos.filho@aluno.unievangelica.edu.br>
2026-03-05 08:52:05 -0300

    Update app.js
    add cors

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ const monitoriaRotas = require('./routes/monitoriaRotas');
 const perfilRotas = require('./routes/perfilRotas');
 
 // 3. Configurar Middlewares Globais
-app.use(cors({ origin: ['https://moni-pro-beta.vercel.app', 'http://127.0.0.1:5500', 'http://localhost:5500'] }));
+app.use(cors({ origin: ['https://moni-pro-b-orm.vercel.app', 'http://127.0.0.1:5500', 'http://localhost:5500'] }));
 app.use(express.json());
 
 // 4. Registrando todas as rotas com seus respectivos prefixos
@@ -22,5 +22,6 @@ app.use('/agendamentos', agendamentoRotas);
 app.use('/disciplinas', disciplinaRotas);
 app.use('/monitorias', monitoriaRotas);
 app.use('/perfil', perfilRotas);
+
 
 module.exports = app;
